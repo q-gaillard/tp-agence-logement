875788cbde23dd28b015dad27a951abb4cbd2e10
q-gaillard <quentingaillard@edouardgand.fr>
2026-03-24 11:30:03 +0100

    création classe logement

---
+++ Logement.cs
@@ -0,0 +1,111 @@
+class Logement
+{
+    protected string reference;
+    protected string adresse;
+    protected int surface;
+    protected double loyerBase;
+    protected bool disponible;
+
+    public string GetReference()
+    {
+        return this.reference;
+    }
+    public string GetAdresse()
+    {
+        return this.adresse;
+    }
+    public int GetSurface()
+    {
+        return this.surface;
+    }
+    public double GetLoyerBase()
+    {
+        return this.loyerBase;
+    }
+    public bool GetDisponible()
+    {
+        return this.disponible;
+    }
+
+    public void SetReference(string reference)
+    {
+        this.reference = reference;
+    }
+    public void SetAdresse(string adresse)
+    {
+        this.adresse = adresse;
+    }
+    public void SetSurface(int surface)
+    {
+        this.surface = surface;
+        if (this.surface < 0)
+        {
+            Console.WriteLine("\n| ! | la surface ne peut pas être négative, elle sera automatiquement remis à 0 | ! |\n");
+            this.surface = 0;
+        }
+    }
+    public void SetLoyerBase(double loyerBase)
+    {
+        this.loyerBase = loyerBase;
+        if (this.loyerBase < 0)
+        {
+            Console.WriteLine("\n| ! | le loyer ne peut pas être négative, il sera automatiquement remis à 0 | ! |\n");
+            this.loyerBase = 0;
+        }
+    }
+    public void SetDisponible(bool disponible)
+    {
+        this.disponible = disponible;
+    }
+
+    public Logement(string reference, string adresse, int surface, double loyerBase, bool disponible)
+    {
+        this.reference = reference;
+        this.adresse = adresse;
+        this.surface = surface;
+        this.loyerBase = loyerBase;
+        this.disponible = disponible;
+        if (this.surface < 0)
+        {
+            Console.WriteLine("\n| ! | la surface ne peut pas être négative, elle sera automatiquement remis à 0 | ! |\n");
+            this.surface = 0;
+        }
+        if (this.loyerBase < 0)
+        {
+            Console.WriteLine("\n| ! | le loyer ne peut pas être négative, il sera automatiquement remis à 0 | ! |\n");
+            this.loyerBase = 0;
+        }
+    }
+
+    public Logement()
+    {
+        this.reference = "pas de référence";
+        this.adresse = "0 rue 'SansRue' 'SansVille";
+        this.surface = 0;
+        this.loyerBase = 0;
+        this.disponible = false;
+    }
+
+    public void Afficher()
+    {
+        Console.WriteLine("\n------- Logement : -------");
+        Console.WriteLine($"réference : {reference}");
+        Console.WriteLine($"adresse : {adresse}");
+        Console.WriteLine($"surface : {surface} m²");
+        Console.WriteLine($"prix du loyer : {loyerBase} euro");
+        if (disponible)
+        {
+            Console.WriteLine("status : disponible");
+        }
+        else
+        {
+            Console.WriteLine("status : indisponile");
+        }
+        Console.WriteLine("--------------------------\n");
+    }
+
+    public double CalculerLoyer()
+    {
+        return loyerBase;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Logement log1 = new Logement("ref 01", "122 rue de la saussice à CrevetteVille", 600, 1200, true);
+        log1.Afficher();
+        Logement log2 = new Logement("ref 02", "3.14 rue QuelquePart à MachinVille", -122, -122, false);
+        log2.Afficher();
     }
 }
